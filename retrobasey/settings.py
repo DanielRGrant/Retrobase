ab7e1fb059571671c8703699aaed864a69dbadc0
Daniel Grant <danielrgrant95@gmail.com>
2020-08-04 17:03:07 +0200

    Ensure github tracks changes to all file

--- retrobasey/settings.py
+++ retrobasey/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%$k#updcpr=75@@i=6j!x*_g5gdkovt(_g4nwb5i#_b((rdpg('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['EC2_DNS_NAME']
 
 
 # Application definition
@@ -127,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
